cbc993a1ed0f11a9c2db165b21b46f40e7a8e3bc
Danil Nigamatyanov <danil_pervik@mail.ru>
2026-04-22 22:29:52 +0500

    Создана структура Model, интерфейс IEntity

---
+++ WinFormsApp1/Model/Entities/GameObjects.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2.Model.Entities
+{
+    internal class GameObject : IEntity
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public GameObject(double X, double Y, double Width, double Height)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public Rectangle GetBounds()
+        {
+            return new Rectangle(X, Y, Width, Height);
+        }
+    }
+}
---
+++ WinFormsApp1/Model/Entities/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2.Model.Entities
+{
+    internal interface IEntity
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle GetBounds();
+    }
+}

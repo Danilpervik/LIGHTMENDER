9b3beb187cd1c1676d62296a4e5a906f985bb4f2
Анна Болтинских <annaboltinskih@Mac.at.urfu.ru>
2026-04-29 15:13:52 +0500

    изменения

--- WinFormsApp1/Model/Entities/EnergyOrb.cs
+++ WinFormsApp1/Model/Entities/EnergyOrb.cs
@@ -8,5 +8,61 @@ namespace WinFormsApp1.Model.Entities
 {
     internal class EnergyOrb
     {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float EnergyAmount { get; set; }
+        public bool IsCollected { get; private set; }
+
+        public EnergyOrb(float x, float y)
+        {
+            X = x;
+            Y = y;
+            Width = 15;
+            Height = 15;
+            EnergyAmount = 10;
+            IsCollected = false;
+        }
+
+        public EnergyOrb(float x, float y, float energyAmount)
+        {
+            X = x;
+            Y = y;
+            Width = 15;
+            Height = 15;
+            EnergyAmount = energyAmount;
+            IsCollected = false;
+        }
+
+        public EnergyOrb(float x, float y, float width, float height, float energyAmount)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            EnergyAmount = energyAmount;
+            IsCollected = false;
+        }
+
+        public float Collect()
+        {
+            if (!IsCollected)
+            {
+                IsCollected = true;
+                return EnergyAmount;
+            }
+            return 0;
+        }
+
+        public bool CanCollect()
+        {
+            return !IsCollected;
+        }
+
+        public void Reset()
+        {
+            IsCollected = false;
+        }
     }
 }
--- WinFormsApp1/Model/Entities/LightSwitch.cs
+++ WinFormsApp1/Model/Entities/LightSwitch.cs
@@ -4,9 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WinFormsApp1.Model.Entities
 {
     internal class LightSwitch
     {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public bool IsActivated { get; private set; }
+        public float ActivationRadius { get; set; }
+
+        public LightSwitch(float x, float y)
+        {
+            X = x;
+            Y = y;
+            Width = 30;
+            Height = 30;
+            IsActivated = false;
+            ActivationRadius = 40;
+        }
+
+        public LightSwitch(float x, float y, float activationRadius)
+        {
+            X = x;
+            Y = y;
+            Width = 30;
+            Height = 30;
+            IsActivated = false;
+            ActivationRadius = activationRadius;
+        }
+
+        public LightSwitch(float x, float y, float width, float height, float activationRadius)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            IsActivated = false;
+            ActivationRadius = activationRadius;
+        }
+
+        public bool Activate()
+        {
+            if (!IsActivated)
+            {
+                IsActivated = true;
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsPlayerInRange(float playerX, float playerY)
+        {
+            float centerX = X + Width / 2;
+            float centerY = Y + Height / 2;
+
+            float dx = centerX - playerX;
+            float dy = centerY - playerY;
+            float distance = (float)Math.Sqrt(dx * dx + dy * dy);
+
+            return distance <= ActivationRadius;
+        }
+
+        public bool IsPlayerInRange(Vector2 playerPosition)
+        {
+            return IsPlayerInRange(playerPosition.X, playerPosition.Y);
+        }
+
+        public void Reset()
+        {
+            IsActivated = false;
+        }
     }
 }
